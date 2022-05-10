c8b2d63e8972f78805638f7a92c9523f0f59da96
exc24 <nadenka18092000@mail.ru>
2022-05-10 18:18:21 +0300

    add food dialog

--- app/src/main/java/com/example/health/screens/food/FoodFragment.kt
+++ app/src/main/java/com/example/health/screens/food/FoodFragment.kt
@@ -1,14 +1,17 @@
 package com.example.health.screens.food
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.health.R
@@ -70,8 +73,47 @@ class FoodFragment : Fragment() {
             val layoutInflater = LayoutInflater.from(APP_ACTIVITY)
             val dialogBinding = AddFoodDialogBinding.inflate(layoutInflater)
 
-            val dialog = AlertDialog.Builder(APP_ACTIVITY)
+            val mContext = APP_ACTIVITY
+            val dialog = AlertDialog.Builder(mContext)
                 .setTitle(foodItem.name)
+                .setView(dialogBinding.root)
+                .setPositiveButton(mContext.getString(R.string.confirm),null)
+                .create()
+            dialog.setOnShowListener {
+                dialogBinding.foodValueEditText.requestFocus()
+                dialogBinding.foodValueSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                        if (fromUser) {
+                            dialogBinding.foodValueEditText.setText(progress.toString())
+                        }
+                    }
+                    override fun onStartTrackingTouch(p0: SeekBar?) {
+                    }
+                    override fun onStopTrackingTouch(p0: SeekBar?) {
+                    }
+                })
+                dialogBinding.foodValueEditText.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    }
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    }
+                    override fun afterTextChanged(textField: Editable?) {
+                        if(textField.toString()!="") {
+                            dialogBinding.foodValueSeekbar.progress = textField.toString().toInt()
+                        }
+                    }
+                })
+                dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener{
+                    val edittext = dialogBinding.foodValueEditText.text.toString()
+                    if(edittext.isBlank()){
+                        dialogBinding.foodValueEditText.error = mContext.getString(R.string.enter_value)
+                        return@setOnClickListener
+                    }
+                    //todo save values
+                    dialog.dismiss()
+                }
+            }
+            dialog.show()
         }
     }
 
